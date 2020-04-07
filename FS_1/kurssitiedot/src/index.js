7050a7665e1ff1b8515c9b412d7edf5d245d4e96
Hipetysmaximus <antti.hippelainen@gmail.com>
2020-04-07 03:37:54 +0300

    1.1 - 1.5

--- FS_1/kurssitiedot/src/index.js
+++ FS_1/kurssitiedot/src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = (header) => {
+   return(
+     <div>
+       <h1>{header.name}</h1>
+     </div>
+   )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Content = (props) => {
+  return (
+    <div>
+      <Part kurssi={props.eka.name} tehtävät={props.eka.exercises} />
+      <Part kurssi={props.toka.name} tehtävät={props.toka.exercises} />
+      <Part kurssi={props.kolmas.name} tehtävät={props.kolmas.exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.kurssi} {props.tehtävät}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.summa}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  const [eka,toka,kolmas] = course.parts
+
+  return (
+    <div>
+      <Header name = {course.name}/>
+      <Content eka={eka} toka={toka} kolmas={kolmas}/>
+      <Total summa = {eka.exercises + toka.exercises + kolmas.exercises}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
