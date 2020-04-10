1bad9c2ac4e2b79e9dc74943a53f5e3b1061fc57
Hipetysmaximus <antti.hippelainen@gmail.com>
2020-04-10 06:36:39 +0300

    2.6 - 2.9

--- FS_2/puhelinluettelo/src/App.js
+++ FS_2/puhelinluettelo/src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Number from './components/Number.js'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchParam, setSearchParam] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchParam(event.target.value)
+  }
+  
+  const addNumber = (event) => {
+    event.preventDefault()
+    const Number = {
+      name: newName,
+      number: newNumber,
+    }
+
+    let nameList = []
+    persons.forEach(person => nameList.push(person.name))
+
+    if (nameList.indexOf(newName) > 0){
+      window.alert(newName + ' is already in the phonebook')
+    } else {
+      setPersons(persons.concat(Number))
+      setNewName("")
+      setNewNumber("")
+    }
+  }
+
+  const peopleToShow = false ? persons : persons.filter(person => person.name.includes(searchParam))
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={searchParam}
+        onChange={handleSearchChange}
+        />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName}
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input value={newNumber}
+          onChange={handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {peopleToShow.map((person) =>
+        <Number key={person.name} person={person}/>
+      )}
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
---
+++ FS_2/puhelinluettelo/src/components/Number.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Number = ({person}) => {
+    return <p>{person.name} {person.number}</p>
+  }
+
+export default Number
